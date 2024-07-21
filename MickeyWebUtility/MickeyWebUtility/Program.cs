c4d403d54e6d65a0d1d7ae9e302e1b30e952bd2c
chuchiang91 <pandachew91@gmail.com>
2024-07-21 10:58:42 +0800

    fix

--- MickeyWebUtility/MickeyWebUtility/Program.cs
+++ MickeyWebUtility/MickeyWebUtility/Program.cs
@@ -3,20 +3,12 @@ using MickeyWebUtility.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.Components.WebAssembly.Services;
-using Microsoft.JSInterop;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Configure base address dynamically
-builder.Services.AddScoped(sp =>
-{
-    var jsRuntime = sp.GetRequiredService<IJSRuntime>();
-    var baseAddress = jsRuntime.InvokeAsync<string>("getBaseHref").Result;
-    return new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress + baseAddress) };
-});
-
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<SGItineraryService>();
 builder.Services.AddScoped<LazyAssemblyLoader>();
 
