233cfb1f59792e621aa20edfdc679956e5f49c9f
chuchiang91 <pandachew91@gmail.com>
2024-07-21 13:52:58 +0800

    fixDeployment

--- MickeyWebUtility/MickeyWebUtility/Program.cs
+++ MickeyWebUtility/MickeyWebUtility/Program.cs
@@ -11,4 +11,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<SGItineraryService>();
 builder.Services.AddScoped<LazyAssemblyLoader>();
+
 await builder.Build().RunAsync();
