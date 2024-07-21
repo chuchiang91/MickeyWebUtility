cf2933fc69c933ac13e25452f474bba9d3f87c23
chuchiang91 <pandachew91@gmail.com>
2024-07-21 13:46:34 +0800

    fix

--- MickeyWebUtility/MickeyWebUtility/Program.cs
+++ MickeyWebUtility/MickeyWebUtility/Program.cs
@@ -2,6 +2,7 @@ using MickeyWebUtility;
 using MickeyWebUtility.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<SGItineraryService>();
-
+builder.Services.AddScoped<LazyAssemblyLoader>();
 await builder.Build().RunAsync();
