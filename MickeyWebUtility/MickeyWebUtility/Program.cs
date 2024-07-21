6d51e13fe40b8ccbab1dbe04348993ffb1c24f04
chuchiang91 <pandachew91@gmail.com>
2024-07-21 10:18:25 +0800

    remove .vs folder

--- MickeyWebUtility/MickeyWebUtility/Program.cs
+++ MickeyWebUtility/MickeyWebUtility/Program.cs
@@ -2,11 +2,21 @@ using MickeyWebUtility;
 using MickeyWebUtility.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+// Configure base address for GitHub Pages
+if (!builder.HostEnvironment.IsDevelopment())
+{
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress + "MickeyWebUtility/") });
+}
+else
+{
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+}
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<SGItineraryService>();
+builder.Services.AddScoped<LazyAssemblyLoader>();
 await builder.Build().RunAsync();
