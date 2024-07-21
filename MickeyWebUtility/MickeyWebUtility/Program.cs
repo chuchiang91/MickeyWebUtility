3eb2aafe8ce5ee0e0d06e591ace708112c17830a
chuchiang91 <pandachew91@gmail.com>
2024-07-21 10:42:12 +0800

    fixGitIssue

--- MickeyWebUtility/MickeyWebUtility/Program.cs
+++ MickeyWebUtility/MickeyWebUtility/Program.cs
@@ -3,20 +3,21 @@ using MickeyWebUtility.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.Components.WebAssembly.Services;
+using Microsoft.JSInterop;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-// Configure base address for GitHub Pages
-if (!builder.HostEnvironment.IsDevelopment())
+
+// Configure base address dynamically
+builder.Services.AddScoped(sp =>
 {
-    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress + "MickeyWebUtility/") });
-}
-else
-{
-    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-}
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+    var jsRuntime = sp.GetRequiredService<IJSRuntime>();
+    var baseAddress = jsRuntime.InvokeAsync<string>("getBaseHref").Result;
+    return new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress + baseAddress) };
+});
+
 builder.Services.AddSingleton<SGItineraryService>();
 builder.Services.AddScoped<LazyAssemblyLoader>();
+
 await builder.Build().RunAsync();
